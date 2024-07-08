9fa64dc62839f382e08fcc39f6da8abccbfb2325
Ali Jardine <jardine64@gmail.com>
2024-07-08 18:50:04 +0100

    adding structure for a command line assembler.

---
+++ AssemblerCommandLine/src/main/java/AssemblerCommandLineMain.java
@@ -0,0 +1,7 @@
+public class AssemblerCommandLineMain {
+    public static void main(String[] args) {
+        System.out.println("std out");
+        System.err.println("std err");
+        System.exit(1);
+    }
+}
