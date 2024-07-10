c520a3c34e5b426592036b95174598cb073f29d0
Ali Jardine <jardine64@gmail.com>
2024-07-10 13:40:55 +0100

    Simple hack assembler that works with files

--- AssemblerCommandLine/src/main/java/AssemblerCommandLineMain.java
+++ AssemblerCommandLine/src/main/java/AssemblerCommandLineMain.java
@@ -1,7 +1,20 @@
+import Hack.Assembler.HackAssembler;
+import Hack.Assembler.HackAssemblerTranslator;
+import Hack.Translators.HackTranslatorException;
+
+import static Hack.Utilities.Definitions.ROM_SIZE;
+
 public class AssemblerCommandLineMain {
     public static void main(String[] args) {
-        System.out.println("std out");
-        System.err.println("std err");
-        System.exit(1);
+        try {
+            new HackAssembler("/Users/alijardine/Documents/Code/nand2tetris-bolt/SimulatorsPackage/src/test/java/edu/uob/invalid.asm",
+                    ROM_SIZE,
+                    HackAssemblerTranslator.NOP,
+                    false);
+            System.exit(0);
+        } catch (HackTranslatorException e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
     }
 }
